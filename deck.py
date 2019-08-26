db32e3d8a8ec4f57c4956edf7caeb0383bfd834a
stephenh67 <sbhope67@gmail.com>
2019-08-26 11:51:56 -0400

    rename shadowed variable, fix whitespace

--- deck.py
+++ deck.py
@@ -2,24 +2,22 @@ import random
 
 # TODO turn this into class Deck
 
-HEARTS   = chr(9829)
+HEARTS = chr(9829)
 DIAMONDS = chr(9830)
-SPADES   = chr(9824)
-CLUBS    = chr(9827)
+SPADES = chr(9824)
+CLUBS = chr(9827)
 
 
 def deck():
     """generates a deck of cards as a tuple (suit, rank)"""
-    deck = []
+    card_deck = []
     for suit in (HEARTS, DIAMONDS, SPADES, CLUBS):
         for rank in range(2, 11):
-            deck.append((str(suit), rank))
+            card_deck.append((str(suit), rank))
         for face_card in ('A', 'J', 'Q', 'K'):
-            deck.append((str(suit), face_card))
-    random.shuffle(deck)
-    return deck
+            card_deck.append((str(suit), face_card))
+    random.shuffle(card_deck)
+    return card_deck
+
 
 print(deck())
-
-
-
