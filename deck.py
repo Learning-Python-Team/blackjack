227fb0989337359249bcc68625e226ac874db6e2
stephenh67 <sbhope67@gmail.com>
2019-08-27 09:18:34 -0400

    add cards_value

--- deck.py
+++ deck.py
@@ -13,13 +13,35 @@ def deck():
     card_deck = []
     for suit in (HEARTS, DIAMONDS, SPADES, CLUBS):
         for rank in range(2, 11):
-            card_deck.append((rank, str(suit)))
+            card_deck.append((str(rank) + str(suit)))
         for face_cards in ('A', 'J', 'Q', 'K'):
-            card_deck.append((face_cards, str(suit)))
+            card_deck.append((str(face_cards) + str(suit)))
     random.shuffle(card_deck)
     return card_deck
 
 
-deck = deck()
+def cards_value(cards):
+    """covert all cards to value adjusting for aces"""
+    card_value = 0
+    aces = 0
+    for card in cards:
+        if card[0] in ['J', 'Q', 'K']:
+            card_value += 10
+        elif card[0] == 'A':
+            aces += 1
+        else:
+            card_value += int(card[0])
 
+    for ace in range(aces):
+        if card_value + 11 <= 21:
+            card_value += 11
+        else:
+            card_value += 1
+    return card_value
+
+# for testing
+deck = deck()
+cards = deck.pop(), deck.pop(), deck.pop()
 print(deck)
+print(cards)
+print(cards_value(cards))
