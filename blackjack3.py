6def7e25ec56b27038145ee6ef86af3fe5c409fd
stephenh67 <sbhope67@gmail.com>
2019-08-27 23:00:47 -0400

    fix natural add TODO 6 deck shoe

--- blackjack3.py
+++ blackjack3.py
@@ -8,6 +8,7 @@ CLUBS = chr(9827)
 
 def create_deck():
     """generates a deck of cards (rank_suit)"""
+    # TODO maka a 6 deck shoe
     card_deck = []
 
     for suit in (HEARTS, DIAMONDS, SPADES, CLUBS):
@@ -76,8 +77,9 @@ def player_action(cards):
     :param cards:
     :return: cards after draw, natural: T/F
     """
+    # TODO break this into functions by action
     print("debug", cards)
-    # check if cards are the same and offer split
+    # check if cards are the same and offer split, tested
     if cards[0][0] == cards[1][0]:
         print("Split? (Y)es (N)o")
         split = input().upper()
@@ -85,11 +87,11 @@ def player_action(cards):
         if split == 'Y':
             player_card_one, player_card_two = cards
             print(player_card_one, player_card_two, "splitting")
-    
-    # checks if first two cards are a natura; 21
+
+    # checks if first two cards are a natural 21, tested
     if cards_value(cards) == 21:
-        # return cards, natural
-        return cards, True
+        # return cards
+        return cards
 
     while True:
         print("(H)it, (S)tick")
@@ -102,7 +104,7 @@ def player_action(cards):
         if action == "S":
             break
     # return cards, natural
-    return cards, False
+    return cards
 
 
 def dealer_action(cards):
@@ -129,16 +131,17 @@ while True:
         print()
 
         # allows player to hit or stick
-        player_cards, natural = player_action(player_cards)
+        player_cards = player_action(player_cards)
 
         # dealer draws card if total card value is less than 16
         if cards_value(dealer_cards) < 16:
             dealer_cards = dealer_action(dealer_cards)
             break
+
         if cards_value(dealer_cards) >= 16:
             break
 
-    if natural:
+    if len(player_cards) == 2 and cards_value(player_cards) == 21:
         # player wins 1.5 * bet
         print("Player wins with a natural 21\n")
 
