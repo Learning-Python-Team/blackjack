fd06bf7ade5d0b3d032ffcf1572047e324dc9ff7
stephenh67 <sbhope67@gmail.com>
2019-08-30 13:26:49 -0400

    demo prints 2 cards to screen

--- blackjack4.py
+++ blackjack4.py
@@ -4,6 +4,7 @@
 """
 
 import pygame
+import cards
 
 # Define some colors
 BLACK = (0, 0, 0)
@@ -14,8 +15,11 @@ RED = (255, 0, 0)
 pygame.init()
 
 # Set the width and height of the screen [width, height]
-size = (1200, 600)
-screen = pygame.display.set_mode(size)
+#size = (1200, 600)
+#screen = pygame.display.set_mode(size)
+display_width = 1000
+display_height = 800
+gameDisplay = pygame.display.set_mode((display_width, display_height))
 
 pygame.display.set_caption("Blackjack")
 
@@ -25,12 +29,25 @@ done = False
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
+card_image = pygame.image.load(cards.card_conversion("2H"))
+card_image2 = pygame.image.load(cards.card_conversion('AC'))
+def card(x,y, card):
+    gameDisplay.blit(card, (x, y))
+
+x = (display_width * 0.45)
+y = (display_height * .1)
+z = (display_width * .55)
+
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+
+    gameDisplay.fill(GREEN)
+    card(x, y, card_image)
+    card(z, y, card_image2)
 
     # --- Game logic should go here
 
@@ -41,7 +58,7 @@ while not done:
 
     # If you want a background image, replace this clear with blit'ing the
     # background image.
-    screen.fill(GREEN)
+    #screen.fill(GREEN)
 
     # --- Drawing code should go here
 
